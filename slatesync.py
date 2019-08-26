ce42c10f3e13b6426dec540e66933042080314f2
glasserd <glasserd@union.edu>
2019-08-26 16:15:33 -0400

    Changed logic to <= today for interview deletion

--- slatesync.py
+++ slatesync.py
@@ -510,7 +510,7 @@ def readSlateCalendarWebService (calendar, slateEventWebService, slateEventWebSe
 					tempEvent['end'] = date(year, month, day)
 
 			# If event is an interview and occurs in the past delete it from the calendar
-			if event['Type'] == 'Interview' and event['Attendees'] == '0' and tempEvent['start'].date() < datetime.now().date():
+			if event['Type'] == 'Interview' and event['Attendees'] == '0' and tempEvent['start'].date() <= datetime.now().date():
 				logger.info('readSlateCalendarWebService - Removing unbooked expired interview %s for calendar %s', event['GUID'], calendar)
 				continue
 
